b34e22b16dc9f74c9663d2d0d9c550da56ddeb61
Hippo Alpha <renegadehippos@pm.me>
2022-06-30 14:32:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./data.js";
+import React from "react"
+import Travel from "./Travel.js";
+import {FaMapMarkerAlt} from 'react-icons/fa';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dataMap = data.map(data => {
+      return(
+      <div className="wrapper">
+      <img src={data.imageUrl}></img>
+      <div className="sub-wrapper">
+        <div className="location-maps">    
+          <p className="location"><FaMapMarkerAlt/>{data.location}</p>
+          <a href={data.googleMapsUrl} target="_blank" className="links">View in google maps</a>    
+        </div>
+
+        <h1>{data.title}</h1>
+        <p className="dates">{data.startDate} - {data.startDate}</p>
+        <p>{data.description}</p>
+      </div>
+      </div>)
+    })
+
+ return (
+  <div>
+      <header>
+        my travel journal
+        </header>
+      {dataMap}
+  </div>
+ )
 }
+
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "JAPAN",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Cappadocia",
+        location: "TURKEY",
+        googleMapsUrl: "https://goo.gl/maps/tF2MkmZMvS8AFQkJ6",
+        startDate: "12 July, 2021",
+        endDate: "24 July, 2021",
+        description: "Cappadocia is a famous place in Turkey. Fairy Chimneys has been shattered around the city and is being constantly visited by tourists.",
+        imageUrl: "https://images.pexels.com/photos/3889728/pexels-photo-3889728.jpeg"
+    },
+    {
+        title: "Gulfoss",
+        location: "ICELAND",
+        googleMapsUrl: "https://goo.gl/maps/nxgwHpTrtg9RPQkq9",
+        startDate: "12 Aug, 2021",
+        endDate: "24 Aug, 2021",
+        description: "Gulfoss is a strong waterfall located in Iceland. A landmark worth a visit if you are in Iceland for sure.",
+        imageUrl: "https://www.thediscoveriesof.com/wp-content/uploads/2019/01/Travel-Bucket-List-68.jpg"
+    }
+]
