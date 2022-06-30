db9293f2e6fd706d4f36237f7d5b27d1da946684
Hippo Alpha <renegadehippos@pm.me>
2022-06-30 16:29:57 +0300

    Updates

---
+++ precache-manifest.11911065c5903737b7ee0a642e1b3af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee4a4c6cff0eea8e4fd4",
+    "url": "/travel-journal/static/css/main.fb088e93.chunk.css"
+  },
+  {
+    "revision": "ee4a4c6cff0eea8e4fd4",
+    "url": "/travel-journal/static/js/main.ee4a4c6c.chunk.js"
+  },
+  {
+    "revision": "c9f5824f8fbb99c89f3d",
+    "url": "/travel-journal/static/js/1.c9f5824f.chunk.js"
+  },
+  {
+    "revision": "6b67c87f7fe4c66fb3a7",
+    "url": "/travel-journal/static/js/runtime~main.6b67c87f.js"
+  },
+  {
+    "revision": "52e2d9af92fd66dc0ef575812a1578bb",
+    "url": "/travel-journal/index.html"
+  }
+];
